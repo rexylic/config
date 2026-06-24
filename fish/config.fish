93cb17c29772f463f07daa6b3bda2a0c3e6eaecd
rexylic <i@rexylic.com>
2026-06-24 12:37:18 -0400

    refactor

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,12 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/conda/bin
 
 source ~/.venv/bin/activate.fish
+
+fzf_configure_bindings --directory=ctrl-f
 
 if status is-interactive
     # Autoswitch light and dark mode

93a97cff8d3d6015025251104f3c1e34c3474cb8
rexylic <i@rexylic.com>
2026-06-23 22:37:18 -0400

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,29 +3,16 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/conda/bin
 
+source ~/.venv/bin/activate.fish
+
 if status is-interactive
     # Autoswitch light and dark mode
     set ALAC_CFG ~/.config/alacritty/alacritty.toml
     set HX_CFG ~/.config/helix/config.toml
     set BG (dark-notify -e)
     if [ $BG = dark ]
-        sed -i 1 s/light/dark/ $HX_CFG
-        sed -i 1 s/.toml/_dark.toml/ $ALAC_CFG
+        sed -i 1 s/light/dark/ $HX_CFG $ALAC_CFG
     else
-        sed -i 1 s/dark/light/ $HX_CFG
-        sed -i 1 s/_dark.toml/.toml/ $ALAC_CFG
+        sed -i 1 s/dark/light/ $HX_CFG $ALAC_CFG
     end
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/rfang/.local/conda/bin/conda
-    eval /Users/rfang/.local/conda/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/Users/rfang/.local/conda/etc/fish/conf.d/conda.fish"
-        . "/Users/rfang/.local/conda/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/Users/rfang/.local/conda/bin" $PATH
-    end
-end
-# <<< conda initialize <<<

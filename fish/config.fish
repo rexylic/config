b6b0a550f5eba820a9b4d21790017f4ccee91551
rexylic <i@rexylic.com>
2026-07-05 16:00:51 -0400

    refined config

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,26 @@
+# Brew shellenv
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# Add path
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/conda/bin
 
-set tide_character_icon '$'
-fzf_configure_bindings --directory=ctrl-f
-
 if status is-interactive
+    # Brew autocompletion
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+
+    # Fuzzy finder binding
+    fzf_configure_bindings --directory=ctrl-f
+
+    # Set prompt icon
+    set tide_character_icon '$'
+
     # Autoswitch light and dark mode
     set ALAC_CFG ~/.config/alacritty/alacritty.toml
     set HX_CFG ~/.config/helix/config.toml

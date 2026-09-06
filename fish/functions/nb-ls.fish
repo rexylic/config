11b3fea9125b558162f309d8213e393318b37c6d
rexylic <i@rexylic.com>
2026-09-06 19:16:40 -0400

    Sort pkg spec

---
+++ fish/functions/nb-ls.fish
@@ -0,0 +1,3 @@
+function nb-ls --wraps='nb list | sort -o ~/.config/nanobrew/bundle' --description 'alias nb-ls nb list | sort -o ~/.config/nanobrew/bundle'
+    nb list | sort -o ~/.config/nanobrew/bundle $argv
+end

c8a7ef45d997fa0e779fdeb1bdcc775b8fbe9903
rexylic <i@rexylic.com>
2026-09-06 19:28:41 -0400

    Rename

--- fish/functions/nb-ls.fish
+++ fish/functions/nb-ls.fish
@@ -1,3 +1,3 @@
-function nb-ls --wraps='nb list | sort -o ~/.config/nanobrew/bundle' --description 'alias nb-ls nb list | sort -o ~/.config/nanobrew/bundle'
-    nb list | sort -o ~/.config/nanobrew/bundle $argv
+function nb-ls --wraps='nb list | sort -o ~/.config/nanobrew/bundle' --description 'alias nb-ls nb list | sort -o ~/.config/manifest'
+    nb list | sort -o ~/.config/manifest $argv
 end

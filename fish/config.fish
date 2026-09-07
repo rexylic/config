e300dbd6e87275703bc673a7e39f561d5dfe6d1e
rexylic <i@rexylic.com>
2026-09-06 20:14:27 -0400

    Stop using mise

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,6 @@ fish_add_path /opt/nanobrew/prefix/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 
-# Setup mise
-mise activate fish | source
-
 # Interactive session only
 if status is-interactive
     # Fuzzy finder binding

251a1eaa7becc261eda29546e23fe86998b38acc
rexylic <i@rexylic.com>
2026-09-13 23:07:26 -0400

    Config ruby

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,13 @@
-# Add path
+# Add nanobrew paths
 fish_add_path /opt/nanobrew/prefix/bin
-fish_add_path ~/.cargo/bin
+fish_add_path /opt/nanobrew/prefix/lib/ruby/gems/4.0.0/bin
+
+# Add local paths
 fish_add_path ~/.local/bin
+fish_add_path ~/.local/share/gem/ruby/4.0.0/bin
+
+# Add cargos
+fish_add_path ~/.cargo/bin
 
 # Interactive session only
 if status is-interactive
